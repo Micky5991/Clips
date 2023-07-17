0ce2b4ad01bf9144c726f315c4d97ec4a76e7646
Francesco Paolocci <micky5991@gmail.com>
2023-07-17 19:25:18 +0200

    Replace vercel favicon with clip emoji

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="favicon.png" />
       <body className={inter.className}>
           <div className={"container mx-auto px-4"}>
               <Navigation />
