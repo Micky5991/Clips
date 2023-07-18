4490a8a1772c625dab5557537ebad415123c1097
Francesco Paolocci <micky5991@gmail.com>
2023-07-18 21:03:37 +0200

    Add description box to clip loading skeleton

--- app/[video]/loading.js
+++ app/[video]/loading.js
@@ -3,7 +3,9 @@ export default function Loading() {
         <div role="status" className="w-full animate-pulse">
             <div className="h-8 my-4 bg-gray-200 rounded-full dark:bg-gray-700 w-96"></div>
 
-            <div className={"w-full my-12 aspect-video bg-gray-200 rounded-lg dark:bg-gray-700 mb-4"}></div>
+            <div className={"w-full my-12 aspect-video bg-gray-200 rounded-lg dark:bg-gray-700"}></div>
+
+            <div className={'bg-zinc-900 p-6 rounded-2xl h-16'}></div>
 
             <span className="sr-only">Loading...</span>
         </div>
