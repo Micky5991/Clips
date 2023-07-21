3baa84a3b9fb5fb58af7083db7d2d668e817362b
Francesco Paolocci <micky5991@gmail.com>
2023-07-21 22:36:59 +0200

    Add global error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {page} from "$app/stores";
+</script>
+
+<div class="text-center">
+    <div class="font-bold text-8xl mb-6">{$page.status}</div>
+    <div class="text-xl">{$page.error.message}</div>
+</div>
