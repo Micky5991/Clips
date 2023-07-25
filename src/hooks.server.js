bc40d9e728b7f563aa6267e6a1633ded0db41db4
Francesco Paolocci <micky5991@gmail.com>
2023-07-25 21:01:18 +0200

    Configure prisma adapter

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Discord from "@auth/core/providers/discord";
 import { env } from '$env/dynamic/private';
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import prisma from "$lib/prisma.js";
 
 export const handle = SvelteKitAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
         Discord({
             clientId: env.DISCORD_ID,
